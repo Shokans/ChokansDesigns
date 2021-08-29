f6da1662ffcc55a68c27f34767e1e88f0599bff5
Shokans <75579951+Shokans@users.noreply.github.com>
2021-08-29 16:31:56 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+// navbar-brand
+document.getElementById("navbar-brand").addEventListener("mouseover", function mouseOver() {
+    document.getElementById("navbar-brand").style.color = "white";
+  });
+document.getElementById("navbar-brand").addEventListener("mouseout", mouseOut);
+
+function mouseOver() {
+  document.getElementById("navbar-brand").style.color = "white";
+}
+
+function mouseOut() {
+  document.getElementById("navbar-brand").style.color = "#134361";
+}
+// navbarlink1
+document.getElementById("navbarlink1").addEventListener("mouseover", function mouseOver1() {
+    document.getElementById("navbarlink1").style.color = "white";
+  });
+document.getElementById("navbarlink1").addEventListener("mouseout", mouseOut1);
+
+function mouseOver1() {
+  document.getElementById("navbarlink1").style.color = "white";
+}
+
+function mouseOut1() {
+  document.getElementById("navbarlink1").style.color = "#134361";
+}
+// navbarlink2
+document.getElementById("navbarlink2").addEventListener("mouseover", function mouseOver2() {
+    document.getElementById("navbarlink2").style.color = "white";
+  });
+document.getElementById("navbarlink2").addEventListener("mouseout", mouseOut2);
+
+function mouseOver2() {
+  document.getElementById("navbarlink2").style.color = "white";
+}
+
+function mouseOut2() {
+  document.getElementById("navbarlink2").style.color = "#134361";
+}
